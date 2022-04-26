8b335163ddbd0597fddc1bbe471445cc55b425bf
Nelson Adarmes <jhoeladarmes@gmail.com>
2022-04-26 10:09:38 -0300

    algoritmo hecho en clase

---
+++ algoritmo.js
@@ -0,0 +1,11 @@
+//datos
+var aceleración = 20
+var velocidadAlcanzar = 60
+var velocidadInicial = 0
+
+var tiempoEnAlcanzarVelocidadReq =  velocidadAlcanzar/aceleración
+
+console.log("El tiempo que tardará en alcanzar 60km/h son: " + tiempoEnAlcanzarVelocidadReq + " horas")
+
+
+
